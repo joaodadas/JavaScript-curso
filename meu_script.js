2220cbb532ef63aa693a8ae31d68cf7df6f4deb8
João Vitor Dadas <dadasjv@hotmail.com>
2022-08-01 10:49:33 -0300

    condicionais

--- meu_script.js
+++ meu_script.js
@@ -6,9 +6,24 @@ var altura = prompt('Informe a sua altura em centimetros: ')
 
  altura /= 100
 
- M = peso / altura**2
+ let clas = 0
+ let m = peso / (altura**2)
 
-if (M < 16) {
-    calss = 'Baxo peso muito grave'
+if (m < 16) {
+    clas = 'Baxo peso muito grave'
+}else if(16 < m && m <= 16.99) {
+    clas = 'Baixo pesso grave'
+}else if(16 < m && m <= 18.49){
+    clas = 'Baixo peso'
+}else if(18.50 < m && m <= 24.99){
+    clas = 'Peso normal'
+}else if(25 < m && m <= 29.99){
+    clas = 'Sobre peso'
+}else if(30 < m && m <= 34.99){
+    clas = 'Obesidade grau 1'
+}else if(35 < m && m <=  39.99){
+    clas = 'Obesidade garu 2'
+}else if(40 < m){
+    clas = 'Obesidade grau 3'
 }
-
+document.write(`${nome} possui índice de masssa corporaç igual a ${m} sendo calssificado como: ${clas}`)
