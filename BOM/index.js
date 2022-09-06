5d69d8c8e20d4556edd9d95744f97a482ac867f3
João Vitor Dadas <dadasjv@hotmail.com>
2022-09-06 15:35:17 -0300

    indice

--- BOM/index.js
+++ BOM/index.js
@@ -1,9 +1,1 @@
-let janela
-
-function abrirPopUp() {
-    janela = window.open('', 'nove-janela', 'width=200, height=100')
-}
-
-function fecharPopUp() {
-    janela.close()
-}
+window.location.href='http://www.google.com'
